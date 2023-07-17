f4c92a27fa5807bef36d0fd9c7c9a697a0d09806
Machina58 <petrick5588@gmail.com>
2023-07-17 12:25:32 +0700

    set up html

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-script.js
+ console.log("123");
+ function hitung(){
+ 	let usia = document.getElementById("usia").value;
+ 	let berat = document.getElementById("berat").value;
+ 	let tinggi = document.getElementById("tinggi").value;
+
+ 	console.log(usia * berat);
+ 	console.log(berat);
+ 	console.log(tinggi);
+ 	document.getElementById("hasilhitungan").innerHTML = usia;
+ 	event.preventDefault(); 
+ }
